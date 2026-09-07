c6704bad1096aaea953379d3722b0e520da5f8a6
Your Name <you@example.com>
2026-09-07 11:56:09 +0300

    1

--- main.c
+++ main.c
@@ -1,23 +1,36 @@
 #include <stdio.h>
 #include <windows.h>
 
-void test() {
+void info() {
     int age = 19;
     double height = 1.78;
     char grade = 'A';
 
     printf("Возраст %d лет\n", age);
     printf("Рост %f см\n", height);
-    printf("Буква %c (%d код)\n", grade);
+    printf("Буква %c (%d код)\n", grade, grade);
 
     printf("sizeof(int) = %zu байт\n", sizeof(int));
-    printf("sizeof(int) = %zu байт\n", sizeof(double));
-    printf("sizeof(int) = %zu байт\n", sizeof(char));
+    printf("sizeof(double) = %zu байт\n", sizeof(double));
+    printf("sizeof(char) = %zu байт\n", sizeof(char));
 }
 
+void test(void) {
+    int number = 0;
+
+    printf("Введите число: ");
+    if (scanf("%d", &number) != 1) {
+        printf("n/a\n");
+        return;
+    }
+    printf("Вы ввели: %d\n", number);
+}
+
+
 int main() {
-    SetConsoleOutputCP(65001);
+    // SetConsoleOutputCP(65001);
     printf("Hello, world!\n");
+    info();
     test();
 
     return 0;
@@ -36,5 +49,11 @@ age   - обращаемся по имени переменной
 &age  - обращаемся к адресу переменной
 *age  - указатель на переменную
 **age - операция разыменоаввания переменной
+
+Для локальных переменных указатели мы не используем
+Указатели используют для ссылки на переменную которая
+
+
+
 
 */
